d5f9c026641699d7954e6e83dc69548e199e4414
Jeroen Doggen <jeroendoggen@gmail.com>
2013-05-23 16:05:16 +0200

    added version for 'manual note taking'

--- latexhandoutsbuilder.py
+++ latexhandoutsbuilder.py
@@ -1,6 +1,6 @@
 """ Build Script for Latex-beamer based course handouts
 
-Use one process, helpful for debugging build errors.
+Use multiple processes where possible to increase build speed
 
 """
 
@@ -15,18 +15,21 @@ import signal
 import glob
 import zipfile
 import zlib
+from multiprocessing import Process
 
 """User configurable settings:
 CONFIG_FILE = the file with the list of chapters
 HANDOUTSPATH = where do we want to place pdf document (and the final archive)
-BOOK_TITLE = title of the handouts book tex file (without .tex)
-ARCHIVE_TITLE = name of the final archive
+BOOK_TITLE = title of the handouts book tex file (without .tex !)
+ARCHIVE_TITLE = name of the final archive (with .zip !)
+BUILD_TIMEOUT = maximum time for a pdflatex build (default: 30s)
 
 """
 CONFIG_FILE = "chapters.conf"
 HANDOUTSPATH = "Handouts"
 BOOK_TITLE = "ExampleHandoutsBook"
 ARCHIVE_TITLE = "ExampleHandoutsBook.zip"
+BUILD_TIMEOUT = 30
 
 """Global variables
 TODO: make them local
@@ -36,7 +39,7 @@ SCRIPTPATH = os.getcwd()
 FAILED_BUILDS = []
 COUNTER = 0
 TOTAL_TASKS = 0
-TASKS_PER_CHAPTER = 8
+TASKS_PER_CHAPTER = 5
 START_TIME = 0
 STOP_TIME = 0
 
@@ -79,11 +82,14 @@ def print_chapters(chapters_list, book_title):
     print ("")
 
 
-def timed_cmd(command, timeout):
+def timed_cmd(command, timeout, path):
     """Call a cmd and kill it after 'timeout' seconds"""
     cmd = command.split(" ")
-    print_progress_counter(command)
-    print (command)
+    #print_progress_counter(command)
+    #print (path)
+    #print (command)
+    os.chdir(path)
+    #print(os.getcwd())
     start = datetime.datetime.now()
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
       stderr=subprocess.PIPE)
@@ -92,6 +98,7 @@ def timed_cmd(command, timeout):
         now = datetime.datetime.now()
         time.sleep(1)
         if (now - start).seconds > timeout:
+            print (command)
             print ("Process timeout")
             os.kill(process.pid, signal.SIGKILL)
             os.waitpid(-1, os.WNOHANG)
@@ -108,59 +115,47 @@ def print_progress_counter(command):
     print (TOTAL_TASKS, end=" ")
 
 
+def builder_task(current_chapter):
+    """Function to build a chapter, started as a process """
+    try:
+        timed_cmd(("pdflatex --output-directory=../" + HANDOUTSPATH + " " + current_chapter
+          + ".tex"), BUILD_TIMEOUT, SCRIPTPATH + "/" + current_chapter)
+        timed_cmd(("pdflatex --output-directory=../" + HANDOUTSPATH + " " + current_chapter
+          + ".tex"), BUILD_TIMEOUT, SCRIPTPATH + "/" + current_chapter)
+        timed_cmd(("pdflatex --output-directory=../" + HANDOUTSPATH + " " + current_chapter
+          + "_pres.tex"), BUILD_TIMEOUT, SCRIPTPATH + "/" + current_chapter)
+        timed_cmd(("pdflatex --output-directory=../" + HANDOUTSPATH + " " + current_chapter
+          + "_pres.tex"), BUILD_TIMEOUT, SCRIPTPATH + "/" + current_chapter)
+        timed_cmd(("pdfjam-slides6up " + current_chapter + ".pdf "
+          + "--nup 2x3 --suffix 6pp -q"), BUILD_TIMEOUT, SCRIPTPATH + "/" + HANDOUTSPATH)
+    except OSError:
+        print("Error: unable to open test folder")
+        print("Check your config file")
+        FAILED_BUILDS.append(current_chapter)
+    try:
+        os.chdir(SCRIPTPATH)
+    except OSError:
+        print("Error: unable to open the script folder")
+        print("This should never happen...")
+
+
 def build_chapters(chapters_list):
     """Build all the chapters and move to handouts folder"""
     for index, current_chapter in enumerate(chapters_list):
-        try:
-            os.chdir(current_chapter)
-            timed_cmd(("pdflatex" + " " + current_chapter), 10)
-            timed_cmd(("pdflatex" + " " + current_chapter), 10)
-            timed_cmd(("pdfjam-slides6up" + " " + current_chapter + ".pdf "
-              + "--nup 2x3 --suffix 6pp -q"), 10)
-            timed_cmd(("mv" + " " + current_chapter + ".pdf"
-              + " " + "../" + HANDOUTSPATH), 10)
-            timed_cmd(("mv" + " " + current_chapter + "-6pp" + ".pdf"
-              + " " + "../" + HANDOUTSPATH), 10)
-            cleanup()
-        except OSError:
-            print("Error: unable to open test folder")
-            print("Check your config file")
-            FAILED_BUILDS.append(current_chapter)
-        try:
-            os.chdir(SCRIPTPATH)
-        except OSError:
-            print("Error: unable to open the script folder")
-            print("This should never happen...")
+        index = Process(target=builder_task, args=(current_chapter,))
+        index.start()
+    #Wait for all processes to finish (or just the one that is started last??)
+    while (index.is_alive()):
+        time.sleep(1)
 
-
-def build_chapters_handouts(chapters_list):
-    """Build all the chapters and move to handouts folder"""
-    for index, current_chapter in enumerate(chapters_list):
-        try:
-            os.chdir(current_chapter)
-            current_chapter = current_chapter + "_handout"
-            timed_cmd(("pdflatex" + " " + current_chapter ), 10)
-            timed_cmd(("pdflatex" + " " + current_chapter ), 10)
-            timed_cmd(("mv" + " " + current_chapter + ".pdf"
-              + " " + "../" + HANDOUTSPATH), 10)
-            cleanup()
-        except OSError:
-            print("Error: unable to open test folder")
-            print("Check your config file")
-            FAILED_BUILDS.append(current_chapter)
-        try:
-            os.chdir(SCRIPTPATH)
-        except OSError:
-            print("Error: unable to open the script folder")
-            print("This should never happen...")
 
 def build_book(book_title):
     """Build the handouts book"""
     try:
+        os.chdir(SCRIPTPATH)
         os.chdir(HANDOUTSPATH)
-        timed_cmd(("pdflatex" + " " + book_title), 10)
-        timed_cmd(("pdflatex" + " " + book_title), 10)
-        cleanup()
+        timed_cmd(("pdflatex" + " " + book_title), BUILD_TIMEOUT, SCRIPTPATH + "/" + HANDOUTSPATH)
+        timed_cmd(("pdflatex" + " " + book_title), BUILD_TIMEOUT, SCRIPTPATH + "/" + HANDOUTSPATH)
     except OSError:
         print("Error: unable build the final book")
         FAILED_BUILDS.append("The book:" + book_title)
@@ -181,7 +176,23 @@ def cleanup():
     for files in types:
         files_grabbed.extend(glob.glob(files))
     for filename in files_grabbed:
+        #print(filename)
         os.remove(filename)
+
+def cleanup_chapters(chapters_list):
+    """Cleanup all the chapters"""
+    for index, current_chapter in enumerate(chapters_list):
+        try:
+            os.chdir(SCRIPTPATH + "/" + current_chapter)
+            cleanup()
+        except OSError:
+            print("Error: unable to open chapter folder: " + current_chapter)
+    try:
+        os.chdir(SCRIPTPATH + "/" + HANDOUTSPATH)
+        cleanup()
+    except OSError:
+        print("Error: unable to open the handouts folder during cleanup")
+        print("This should never happen...")
 
 
 def create_archive(chapters_list):
@@ -198,7 +209,7 @@ def create_archive(chapters_list):
                 #os.remove(current_chapter + ".pdf")
                 #os.remove(current_chapter + "-6pp.pdf")
             archive.write(BOOK_TITLE + ".pdf", compress_type=compression)
-            os.remove(BOOK_TITLE + ".pdf")
+            #os.remove(BOOK_TITLE + ".pdf")
         finally:
             archive.close()
     except OSError:
@@ -232,10 +243,11 @@ def run():
     TOTAL_TASKS = TASKS_PER_CHAPTER * count_chapters(chapters_list) + 2 + 1
     print_chapters(chapters_list, BOOK_TITLE)
     build_chapters(chapters_list,)
-    build_chapters_handouts(chapters_list,)
     build_book(BOOK_TITLE)
     create_archive(chapters_list)
     print_summary(timing("stop"))
+    #time.sleep(1)
+    cleanup_chapters(chapters_list)
     return(0)
 
 
